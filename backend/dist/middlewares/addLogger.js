0fbbfc2c7e57fc0b2baf3dae00bc3247dda03bc3
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-10-13 12:54:10 -0300

    cambios en frontend

--- backend/dist/middlewares/addLogger.js
+++
@@ -1,8 +0,0 @@
-import logger from "../utils/logger.js";
-const addLogger = (req, _res, next) => {
-    req.infoPeticion = `${new Date()} | Petición a la ruta '${req.url}' | Método ${req.method}`;
-    req.logger = logger;
-    req.logger.info(req.infoPeticion);
-    next();
-};
-export default addLogger;
