069952264cd8c6d2307ea3cb909863bd62f1e91e
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-10-14 10:30:20 -0300

    agregado de la seccion home y el navbar, con los link y rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
 
-
   return (
-    <>
-      <h1>Hola mundo!</h1>
-    </>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
