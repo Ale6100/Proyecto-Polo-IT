692c10bb8e44c3becb91d907be7c829835bfb155
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-11-17 16:36:36 -0300

    agregado de la seccion nosotros y el inicio, con sus archivos css

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./about.css";
+
+const About = () => {
+    return (
+        <div>
+            hola soy la seccion sobre nosotros
+        </div>
+    );
+}
+
+export default About;
---
+++ frontend/src/pages/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './inicio.css';
+
+const Inicio = () => {
+    return (
+        <div>
+            hola soy la seccion inicio
+        </div>
+    );
+}
+
+export default Inicio;
