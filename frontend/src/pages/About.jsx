424be99be7f4771e84971c049509b6a6a0207206
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-11-19 22:23:00 -0300

    correción de errores de ortografia y cambio de contenido en la seccion inicio y contacto

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,14 +10,14 @@ const About = () => {
     
     return (
         <section className='section__about-container'>
-            <h1>¿A que nos dedicamos?</h1>
+            <h1>¿A qué nos dedicamos?</h1>
             <div className='about__info'>
-                En nuestra plataforma web, nos apasiona brindarte asistencia para descubrir la empresa que mejor satisfaga tus necesidades o simplemente para conocer más sobre sus servicios, independientemente del sector. Contamos con un avanzado sistema de filtros que nos permite refinar la búsqueda y ofrecerte opciones que se alineen perfectamente con lo que estás buscando.
+                En nuestra plataforma web, brindamos asistencia para descubrir la empresa que mejor satisfaga tus necesidades o simplemente para conocer más sobre sus servicios, independientemente del sector. Contamos con un avanzado sistema de filtros que nos permite refinar la búsqueda y ofrecerte opciones que se alineen perfectamente con lo que estás buscando.
             </div>
             <div className='about__valores'>
                 <div>
-                    <h2 className='subtitle__about'>Nuestro valor</h2>
-                    <p>Nuestro compromiso es simplificar el proceso de selección de empresas, garantizando que encuentres la solución que se ajuste a tus expectativas y requerimientos.</p>
+                    <h2 className='subtitle__about'>Nuestro compromiso</h2>
+                    <p>Nos comprometemos a simplificar el proceso de selección de empresas, garantizando que encuentres la solución que se ajuste a tus expectativas y requerimientos.</p>
                 </div>
                 <figure className='container__image image-valores'>
                      <img src="https://s1.significados.com/foto/shutterstock-306587402_bg.jpg" alt="varias personas juntado las manos" />
@@ -29,7 +29,7 @@ const About = () => {
                 </figure>
                 <div>
                     <h2 className='subtitle__about'>El servicio que ofrecemos</h2>
-                    <p> Ya sea que estés buscando servicios en tecnología, salud, finanzas consultorias u otros sectores, nuestros filtros te permitirán explorar una variedad de opciones, aplicando diferentes filtros que se encontraran en el lado izquierdo de la pantalla</p>
+                    <p> Ya sea que estés buscando servicios en tecnología, salud, finanzas consultorias u otros sectores, nuestros filtros te permitirán explorar una gran variedad de opciones</p>
                     <Link className='link-services' to='/company/1'><Button contenido='ver empresas' styleIdContainer='btn_about-container' styleIdBtn='btn_about'/></Link>
                 </div>
             </div>
